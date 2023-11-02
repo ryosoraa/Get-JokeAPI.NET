9675824acd90864e7e45775e113a00f7259429e9
ryosyntaxx <riodwi12174@gmail.com>
2023-11-03 01:22:40 +0700

    first commit

---
+++ src/app/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Collections;
+using System.Threading;
+
+namespace Get_JokeAPi
+{
+    class API
+    {
+        public static String path = @"D:\playground\C#\Get-JokeAPi.NET\src\json\Raw.json";
+        public static StreamWriter writer = File.CreateText(path);
+
+        static void Main(string[] args)
+        {
+
+            string sURL;
+            sURL = "https://candaan-api.vercel.app/api/text";
+
+            WebRequest request;
+            request = WebRequest.Create(sURL);
+
+            Stream objStream;
+            objStream = request.GetResponse().GetResponseStream();
+
+            StreamReader objReader = new StreamReader(objStream);
+            API.writer.WriteLine(objReader.ReadToEnd());
+            Thread.Sleep(5000);
+            Console.WriteLine(objReader.ReadLine());
+            objStream.Close();
+
+            while(objReader != null)
+            {
+                Console.WriteLine(objReader.ReadLine());
+            }
+
+
+        }
+    }
+}
+
---
+++ src/model/DTO/JokesDTO.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Get_JokeAPi.NET.src.model.DTO
+{
+    public class JokesDTO
+    {
+        private string status
+        {
+            get; set;
+        }
+
+        private string total
+        {
+            get; set;
+        }
+
+        private string end_point
+        {
+            get; set;
+        }
+
+        private string method
+        {
+            get; set;
+        }
+
+        private List<string> data
+        {
+            get; set;
+        }
+
+        public JokesDTO(string status, string total, string end_point, string method, List<string> data)
+        {
+
+            this.status = status;
+            this.total = total;
+            this.end_point = end_point;
+            this.method = method;
+            this.data = data;
+
+        }
+
+        public JokesDTO() { }
+    }
+}
